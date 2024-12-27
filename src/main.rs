6bc97b34f2df4ce5e4bb8a6dd0f5b0f97b61e5f4
tbizetde@gmail.com <tbizetde@gmail.com>
2024-12-26 22:57:58 -0300

    streaming screen

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,131 @@
-use device_query::{mouse_state::MouseState, DeviceQuery, DeviceState, Keycode};
+extern crate ffmpeg_next as ffmpeg;
+use minifb::{Key, Scale, ScaleMode, Window, WindowOptions};
+use rayon::prelude::*;
+use rdev::{listen, Event};
 use scrap::{Capturer, Display};
-use std::sync::mpsc::Receiver;
-use std::thread;
-use std::{error::Error, fmt::Debug};
-use winit::{
-    application::ApplicationHandler,
-    error::EventLoopError,
-    event::{StartCause, WindowEvent},
-    event_loop::{self, ActiveEventLoop, EventLoop},
-    window::{Window, WindowAttributes, WindowId},
+use std::{
+    error::Error,
+    sync::{Arc, Condvar, Mutex},
+    thread,
+    time::Duration,
 };
 
-#[derive(Default)]
-struct RSGlitch {
-    window: Option<Window>,
-}
-
-impl<T: 'static + Debug> ApplicationHandler<T> for RSGlitch {
-    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        if self.window.is_none() {
-            self.window = match event_loop
-                .create_window(WindowAttributes::default().with_title("RSGLITCH"))
-            {
-                Ok(window) => Some(window),
-                Err(e) => panic!("Error creating window: {e}"),
-            };
-        }
-    }
-
-    fn window_event(
-        &mut self,
-        _event_loop: &ActiveEventLoop,
-        window_id: WindowId,
-        event: WindowEvent,
-    ) {
-        if let Some(window) = &self.window {
-            if window.id() == window_id {
-                match event {
-                    WindowEvent::CloseRequested => {
-                        self.window = None;
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
-}
-
-fn monitor_input() {
-    let device_state = DeviceState::new();
-    let mut last_mouse_state: Option<MouseState> = None;
+fn capture_screen(
+    shared_buffer: Arc<Mutex<Vec<u32>>>,
+    condvar: Arc<Condvar>,
+    stop_signal: Arc<Mutex<bool>>,
+) -> Result<(), Box<dyn Error>> {
+    let display = Display::primary()?;
+    let mut capturer = Capturer::new(display)?;
+    let width = capturer.width();
+    let height = capturer.height();
 
     loop {
-        let keys = device_state.get_keys();
-        let mouse_state = device_state.get_mouse();
-        println!("Keys: {keys:?}");
-        println!("Last mouse state: {last_mouse_state:?}");
-        println!("mouse state: {mouse_state:?}");
-        for key in keys {
-            match key {
-                _ => {}
-            }
+        if *stop_signal.lock().unwrap() {
+            break;
         }
 
-        last_mouse_state = Some(mouse_state);
+        match capturer.frame() {
+            Ok(frame) => {
+                // convert frame from BGRA to ARGB for minifb
+                let mut buffer = vec![0; width * height];
+                buffer
+                    .par_chunks_mut(width.into())
+                    .enumerate()
+                    .for_each(|(y, row)| {
+                        for x in 0..width {
+                            let idx = (y * width + x) * 4;
+                            let b = frame[idx];
+                            let g = frame[idx + 1];
+                            let r = frame[idx + 2];
+                            row[x] =
+                                0xFF000000 | ((r as u32) << 16) | ((g as u32) << 8) | (b as u32);
+                        }
+                    });
+
+                if let Ok(mut shared) = shared_buffer.lock() {
+                    std::mem::swap(&mut *shared, &mut buffer);
+                    condvar.notify_all();
+                } else {
+                    eprintln!("Failed o lock shared buffer for writing");
+                }
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                println!("Would block error: {e}");
+                thread::sleep(Duration::from_millis(10));
+            }
+            Err(e) => {
+                eprintln!("Error capturing frame: {e}");
+                break;
+            }
+        }
     }
+
+    Ok(())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // global input monitoring
-    thread::spawn(|| {
-        monitor_input();
+    const WIDTH: usize = 1920;
+    const HEIGHT: usize = 1080;
+
+    let shared_buffer = Arc::new(Mutex::new(vec![0; WIDTH * HEIGHT]));
+    let condvar = Arc::new(Condvar::new());
+    let stop_signal = Arc::new(Mutex::new(false));
+
+    // screen capture thread
+    let capture_buffer = Arc::clone(&shared_buffer);
+    let condvar_clone = Arc::clone(&condvar);
+    let stop_signal_clone = Arc::new(Mutex::new(false));
+    thread::spawn(move || {
+        if let Err(e) = capture_screen(capture_buffer, condvar_clone, stop_signal_clone) {
+            eprintln!("Screen capture error: {e:?}");
+        }
     });
 
-    // setup screen capturing
-    let display = Display::primary()?;
-    let mut capturer = Capturer::new(display)?;
+    // global input monitoring thread
+    thread::spawn(|| {
+        if let Err(e) = listen(|_event: Event| {
+            // println!("Global event: {event:?}");
+        }) {
+            eprintln!("Event error: {e:?}");
+        }
+    });
 
-    // setup rendering window
-    let event_loop = EventLoop::new().unwrap();
-    match event_loop.run_app(&mut (RSGlitch { window: None })) {
-        Ok(_) => Ok(()),
-        Err(e) => panic!("Event loop failure"),
+    // rendering window
+    let window_options = WindowOptions {
+        borderless: false,
+        title: true,
+        resize: true,
+        scale: Scale::X1,
+        scale_mode: ScaleMode::Center,
+        topmost: false,
+        transparency: false,
+        none: false,
+    };
+    let mut window = Window::new("RSGLITCH", WIDTH, HEIGHT, window_options)?;
+
+    window.set_target_fps(60);
+
+    // rendering thread
+    loop {
+        if !window.is_open() || window.is_key_down(Key::Q) {
+            *stop_signal.lock().unwrap() = true;
+            break;
+        }
+
+        let mut shared = shared_buffer.lock().unwrap();
+
+        while shared.is_empty() {
+            shared = condvar.wait(shared).unwrap();
+        }
+
+        if let Err(e) = window.update_with_buffer(&shared, WIDTH, HEIGHT) {
+            eprintln!("Error updating window with buffer: {e}");
+            break;
+        }
+
+        shared.clear();
     }
+
+    Ok(())
 }
